1264314af340965f2fea9925159ee34cada50338
David <defaultik.personal@gmail.com>
2024-08-28 09:48:55 +0300

    Flags support added

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main() {
-    system("python \"C:/Program Files/SQLMap/sqlmap.py\"");
+int main(int argc, char* argv[]) {
+    char command[1024] = "python \"C:/Program Files/SQLMap/sqlmap.py\"";
+
+    for (int i = 1; i < argc; i++) {
+        strcat(command, " ");
+        strcat(command, argv[i]);
+    }
+
+    system(command);
+
+    return 0;
 }
